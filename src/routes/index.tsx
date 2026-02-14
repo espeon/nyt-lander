76dbb1b820be3fc8fac26155b422e7dd418ff751
Natalie B. <22222885+espeon@users.noreply.github.com>
2026-02-14 15:25:33 -0600

    for nobody

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,9 +8,12 @@ function App() {
   return (
     <>
       <ShaderBackground />
-      <main className="relative min-h-screen flex items-center justify-center px-6">
-        <div className="max-w-4xl w-full text-start">
-          <p className="text-4xl sm:text-6xl md:text-9xl font-semibold tracking-wide text-balance text-start mx-auto">
+      <main className="relative min-h-screen flex items-center justify-center px-8">
+        <div className="absolute bottom-5 left-auto right-auto">
+          made with less than three in the USA
+        </div>
+        <div className="max-w-3xl w-full text-start">
+          <p className="text-5xl sm:text-8xl md:text-9xl font-semibold tracking-wide text-balance text-start mx-auto">
             <StaggeredText
               animationOffset={0.2}
               once
@@ -63,11 +66,26 @@ function App() {
               ]}
             />
           </p>
-          <StaggeredText
-            delay={1.75}
-            animationOffset={0.2}
-            text={[<p className="font-cursive text-4xl mt-8">nyt</p>]}
-          />
+          <div className="flex flex-col md:flex-row justify-between mt-12 gap-4">
+            <StaggeredText
+              delay={1.75}
+              animationOffset={0.2}
+              className="text-3xl sm:text-5xl text-gray-300"
+              text="for nobody."
+            />
+            <StaggeredText
+              delay={2.75}
+              animationOffset={0.2}
+              className="text-3xl sm:text-4xl text-gray-300"
+              text={[
+                {
+                  element: (
+                    <p className="font-cursive text-4xl ml-auto w-max">nyt</p>
+                  ),
+                },
+              ]}
+            />
+          </div>
         </div>
       </main>
     </>
